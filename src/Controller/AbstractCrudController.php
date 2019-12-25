4153e6b999e548692f8248bb6a1f92de1c9355e2
kot <kostiaGm@gmail.com>
2019-12-25 06:48:29 +0200

    crud abstract class step2

--- src/Controller/AbstractCrudController.php
+++ src/Controller/AbstractCrudController.php
@@ -6,6 +6,7 @@ namespace Symka\Core\Controller;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query;
+use Knp\Component\Pager\PaginatorInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -13,15 +14,73 @@ use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symka\Core\Exception\CrudControllerException;
-use Symka\Core\Interfaces\EntityCrudInterface;
-use Symka\Core\PaginatorInterface;
+use Symka\Core\Interfaces\CrudControllerInterface;
+use Symka\Core\Interfaces\CrudGridInterface;
+use Symka\Core\Interfaces\CrudEntityInterface;
 
-abstract class AbstractCrudController extends AbstractController
+
+abstract class AbstractCrudController extends AbstractController implements CrudControllerInterface, CrudEntityInterface
 {
-
-    protected function save(EntityCrudInterface $entity, Request $request, FormTypeInterface $formType, EntityManagerInterface $entityManager) : FormTypeInterface
+    public function indexAction(Request $request, PaginatorInterface $paginator): Response
     {
-        $form = $this->createForm(get_class($formType), $entity);
+        $result = [];
+
+        if ($this instanceof CrudGridInterface) {
+            $result['paginator'] = $paginator->paginate($this->getGridQuery(), $request->query->getInt('page', 1));
+        }
+
+        return $this->render($this->getTemplateDir(__FUNCTION__), $result);
+    }
+
+    public function saveAction(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger, ?int $id = null): Response
+    {
+        $result = ['form' => null];
+
+        if ($this instanceof CrudSaveInterface) {
+            try {
+                $entity = $this->getEntity($entityManager, $id);
+                $form = $this->save($request, $entity, $entityManager);
+                return $this->redirectToRoute($this->getRouteRedirectAfterSave($id));
+            } catch (CrudControllerException $crudControllerException) {
+                if ($crudControllerException->getCode() != CrudControllerException::ERROR_VALIDATE) {
+                    $logger->error("Error Save Crud ", [
+                        "class" => get_class($this),
+                        "code" => $crudControllerException->getCode(),
+                        "message" => $crudControllerException->getMessage()
+                    ]);
+                }
+            } catch (\Exception $exception) {
+                throw $exception;
+            }
+
+            $result = [
+                'form' => $form
+            ];
+        }
+
+        return $this->render($this->getTemplateDir(__FUNCTION__), $result);
+
+    }
+
+    public function getEntity(EntityManagerInterface $entityManager, ?int $id): CrudEntityInterface
+    {
+        $entityClassName = $this->getClassName();
+        if (!empty($id) && $id > 0) {
+            $entity = $entityManager->find($entityClassName, $id);
+            if (!$entity) {
+                throw $this->createNotFoundException();
+            }
+        } else {
+            $entity = new $entityClassName();
+        }
+
+        return $entity;
+    }
+
+    protected function save(Request $request, CrudEntityInterface $entityCrud, EntityManagerInterface $entityManager): FormTypeInterface
+    {
+        $formType = $this->getFormType();
+        $form = $this->createForm(get_class($formType), $entityCrud);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
@@ -45,8 +104,29 @@ abstract class AbstractCrudController extends AbstractController
         return $formType;
     }
 
-    protected function formData(EntityCrudInterface $data)
+    public function formData(CrudEntityInterface $data): void
     {
 
+    }
+
+    public function getDefaultTemplateNamespace(): string
+    {
+        return '@SymkaCoreBundle/Resource/views';
+    }
+
+    public function getTemplateDir(string $actionName): string
+    {
+        $classShortName = (new \ReflectionClass($this))->getShortName();
+        $classShortName = str_replace('Controller', '', $classShortName);
+        $classShortName = lcfirst($classShortName);
+
+        $templateFileName = str_replace('Action', '', $actionName);
+
+        return $this->getDefaultTemplateNamespace() . '/' . $classShortName . '/' . $templateFileName . $this->getDefaultTemplateExtension();
+    }
+
+    public function getDefaultTemplateExtension(): string
+    {
+        return ".html.twig";
     }
 }
